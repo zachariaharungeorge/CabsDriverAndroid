3bf8bbfa4e0ddf5e6521d30651d943106792f708
Arun George <agz@live.in>
2016-04-27 23:48:25 +0530

    Changes done for adding Login, SignUp and Location Service

--- app/src/main/java/rdxcabs/com/cabsdriverandroid/MainActivity.java
+++ app/src/main/java/rdxcabs/com/cabsdriverandroid/MainActivity.java
@@ -1,9 +1,18 @@
 package rdxcabs.com.cabsdriverandroid;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import rdxcabs.com.cabsdriverandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +20,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final GPSTracker gps = new GPSTracker(MainActivity.this);
+        if(!gps.canGetLocation){
+            AlertDialog.Builder alertDialog=new AlertDialog.Builder(MainActivity.this);
+            alertDialog.setTitle("Location Services not Enabled");
+            alertDialog.setMessage("GPS is not enabled. Do you want to go to Settings and Enable it");
+            alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+
+            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            alertDialog.show();
+        }
+
+        SharedPreferences sp=getSharedPreferences("username",0);
+        String username = sp.getString("username","");
+
+
+        if(username!=""){
+            Intent intent = new Intent(this,TripList.class);
+            startActivity(intent);
+        }
+
+        Button signIn = (Button) findViewById(R.id.login);
+        Button signUp = (Button) findViewById(R.id.signUp);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -23,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
